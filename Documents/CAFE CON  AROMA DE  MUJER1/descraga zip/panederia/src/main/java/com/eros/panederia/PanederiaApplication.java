123540c38a222d4bc439d35383de5ecd0f11eb0e
yecid camilo sierra <sammyvsk@hotmail.com>
2025-07-27 21:30:36 -0300

    Agrego archivos al staging, incluyendo Dockerfile

---
+++ Documents/CAFE CON  AROMA DE  MUJER1/descraga zip/panederia/src/main/java/com/eros/panederia/PanederiaApplication.java
@@ -0,0 +1,13 @@
+package com.eros.panederia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanederiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanederiaApplication.class, args);
+	}
+
+}
